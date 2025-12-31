73c409558c3c00132b7a0567673cba1bd2558a6e
angel_febles <licenciadofebles@gmail.com>
2025-12-31 09:33:34 -0600

    Implemented feedback notifications with toastr

--- public/scripts/get-user-input.js
+++ public/scripts/get-user-input.js
@@ -15,6 +15,24 @@ class TableEntry {
 
     static get_user_input() {
 
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-bottom-full-width",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
         const name = document.getElementById("input_name");
         const type = document.getElementById("input_type");
         const language = document.getElementById("input_language");
@@ -35,35 +53,39 @@ class TableEntry {
 document.getElementById("button-submit").addEventListener("click",
     async function () {
         const new_entry = TableEntry.get_user_input();
-        // console.log(new_entry);
+        // console.log(new_entry.name.length);
+
+        if (new_entry.name.length == 0) {
+            toastr.error("Media has an empty name.");
+
+        } else {
+            try {
+                const response = await fetch("http://localhost:3000/api/media", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(new_entry)
+                });
+
+                const result = await response.json();
+                // console.log("Server response:", result);
 
 
-        var myNotification = new Notify('Yo dawg!', {
-            body: 'This is an awesome notification',
-            notifyShow: onNotifyShow
-        });
 
-        function onNotifyShow() {
-            console.log('notification was shown!');
+                toastr.success("New Entry Added!!!!");
+
+            } catch (error) {
+                console.error("Error sending data:", error);
+                toastr.error("Error sending data, check the logs for more information.");
+            }
+
         }
 
-        try {
-            const response = await fetch("http://localhost:3000/api/media", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(new_entry)
-            });
 
-            const result = await response.json();
-            console.log("Server response:", result);
 
-        } catch (error) {
-            console.error("Error sending data:", error);
-        }
 
-        alert("Done!")
+        // alert("Done!")
     }
 
 )
